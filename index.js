6948f971ce91b641f52eb58cd444c66ca8d72ae3
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2021-09-18 04:02:23 -0700

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,61 @@
+// Question 1:
+//write a function which returns array. call the function and destructure array values and print the value.
+//.....................................................................................................
+
+// function myFunction (){
+//     let arr = ["Nabia","Sheikh",24,] ;
+//     return arr ;
+// }
+// let [firstName , lastName , age] = myFunction() ;
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+
+// Question 2:
+// Make a car object with model, year, color. display all the values by destructuring
+//....................................................................................
+
+// let car = {
+//     model : "Honda" ,
+//     color : "Grey" ,
+//     year : 2021
+// }
+
+// let {model , color , year} = car ;
+// console.log(model);
+// console.log(color);
+// console.log(year);
+
+// Question 3:
+// use three variable name, greeting and display value using template literals
+//...................................................................................
+
+// let fName = "Nabia";
+// let lName = "Sheikh";
+// let qual = "unsergraduate"
+
+// console.log(`my first name is ${fName} 
+// my last name is ${lName}
+// and my qualification is ${qual}`);
+
+
+// Question 4:
+// use array [1,2,3,4,5,6,7,8,9,10] assign first three values in seperate variables and all remaining in one element as an array
+//..........................................................................................................
+
+// const myArry = [1,2,3,4,5,6,7,8,9,10];
+// let [ele1,ele2,ele3,...rest] = myArry ; 
+// console.log(ele1);
+// console.log(ele2);
+// console.log(ele3);
+// console.log(rest);
+
+// Question 5:
+// if car speed is less than 80 display "car speed is slow", if greater than 80 but less than 120 than display 
+// " car speed is moderate" and if greater than 120 display "car speed is fast"
+//......................................................................
+
+// let carSpeed = +prompt("enter car speed");
+// carSpeed < 80 ? console.log("car speed is slow") : carSpeed < 120 ? console.log("car speed is moderate") : console.log("car speed is fast");
+
+
